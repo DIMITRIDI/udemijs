8340e4965827b659da5cae2ab34cc912d21fdec6
Dmitry <dva160972@gmail.com>
2021-03-09 12:27:24 +0300

    Events on mobile devices

--- js/script.js
+++ js/script.js
@@ -1,108 +1,37 @@
-/* Задания на урок:
-
-1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
-новый фильм добавляется в список. Страница не должна перезагружаться.
-Новый фильм должен добавляться в movieDB.movies.
-Для получения доступа к значению input - обращаемся к нему как input.value;
-P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
-
-2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
-
-3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
-
-4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
-"Добавляем любимый фильм"
-
-5) Фильмы должны быть отсортированы по алфавиту */
-
 'use strict';
 
-document.addEventListener('DOMContentLoaded', () => {
+// touchstart - при начальном касании элемента
+// touchmove - при движении пальца по элементу
+// touchend - при отрыве пальца от элемента
+// touchenter - когда мы ведем по экрану и попадаем на элемент, на который навешено это событие
+// touchleave - когда палец продолжил движение по экрану и ушел за пределы элемента с событием
+// touchcancel - когда точка сопрекосновения больше не регистрируется на поверхности экрана
 
-    const movieDB = {
-        movies: [
-            "Логан",
-            "Лига справедливости",
-            "Ла-ла лэнд",
-            "Одержимость",
-            "Скотт Пилигрим против..."
-        ]
-    };
+window.addEventListener('DOMContentLoaded', () => {
+	const box = document.querySelector('.box');
 
-    const adv = document.querySelectorAll('.promo__adv img'),
-        poster = document.querySelector('.promo__bg'),
-        genre = poster.querySelector('.promo__genre'),
-        movieList = document.querySelector('.promo__interactive-list'),
-        addForm = document.querySelector('form.add'),
-        addInput = addForm.querySelector('.adding__input'),
-        checkbox = addForm.querySelector('[type="checkbox"]');
+	box.addEventListener('touchstart', (e) => {
+		e.preventDefault();
 
-    addForm.addEventListener('submit', (event) => {
-        event.preventDefault();
+		console.log('Start');
+		console.log(e.targetTouches);
+	});
 
-        let newFilm = addInput.value;
-        const favorite = checkbox.checked;
+	box.addEventListener('touchmove', (e) => {
+		e.preventDefault();
 
-        if (newFilm) {
+		console.log(e.targetTouches[0].pageX);
+	});
 
-            if (newFilm.length > 21) {
-                newFilm = `${newFilm.substring(0, 22)}...`;
-            }
+	box.addEventListener('touchend', (e) => {
+		e.preventDefault();
 
-            if (favorite) {
-                console.log("Добавляем любимый фильм");
-            }
-
-            movieDB.movies.push(newFilm);
-            sortArr(movieDB.movies);
-    
-            createMovieList(movieDB.movies, movieList);
-        }
-
-        event.target.reset();
-
-    });
-
-    const deleteAdv = (arr) => {
-        arr.forEach(item => {
-            item.remove();
-        });
-    };
-
-    const makeChanges = () => {
-        genre.textContent = 'драма';
-
-        poster.style.backgroundImage = 'url("img/bg.jpg")';
-    };
-
-    const sortArr = (arr) => {
-        arr.sort();
-    };
-
-    function createMovieList(films, parent) {
-        parent.innerHTML = "";
-        sortArr(films);
-    
-        films.forEach((film, i) => {
-            parent.innerHTML += `
-                <li class="promo__interactive-item">${i + 1} ${film}
-                    <div class="delete"></div>
-                </li>
-            `;
-        });
-
-        document.querySelectorAll('.delete').forEach((btn, i) => {
-            btn.addEventListener('click', () => {
-                btn.parentElement.remove();
-                movieDB.movies.splice(i, 1);
-
-                createMovieList(films, parent);
-            });
-        });
-    }
-
-    deleteAdv(adv);
-    makeChanges();
-    createMovieList(movieDB.movies, movieList);
-
+		console.log('End');
+	});
 });
+
+// touches - список всех пальцев, которые взаимодействуют с экраном
+
+// targetTouches - количество пальцев взаимодействующих с конкретным элементом
+
+// changedTouches - список пальцев, участвующих в текущем событии
