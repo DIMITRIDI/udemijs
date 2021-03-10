a87c86c104a55b3902766937f9cce9dbb22923e2
Dmitry <dva160972@gmail.com>
2021-03-10 11:13:07 +0300

    Async, defer, dynamic scripts

--- js/script.js
+++ js/script.js
@@ -1,37 +1,14 @@
 'use strict';
 
-// touchstart - при начальном касании элемента
-// touchmove - при движении пальца по элементу
-// touchend - при отрыве пальца от элемента
-// touchenter - когда мы ведем по экрану и попадаем на элемент, на который навешено это событие
-// touchleave - когда палец продолжил движение по экрану и ушел за пределы элемента с событием
-// touchcancel - когда точка сопрекосновения больше не регистрируется на поверхности экрана
+const p = document.querySelectorAll('p');
+console.log(p);
 
-window.addEventListener('DOMContentLoaded', () => {
-	const box = document.querySelector('.box');
+function loadScript(src) {
+	const script = document.createElement('script');
+script.src = src;
+script.async = false;
+document.body.append(script);
+}
 
-	box.addEventListener('touchstart', (e) => {
-		e.preventDefault();
-
-		console.log('Start');
-		console.log(e.targetTouches);
-	});
-
-	box.addEventListener('touchmove', (e) => {
-		e.preventDefault();
-
-		console.log(e.targetTouches[0].pageX);
-	});
-
-	box.addEventListener('touchend', (e) => {
-		e.preventDefault();
-
-		console.log('End');
-	});
-});
-
-// touches - список всех пальцев, которые взаимодействуют с экраном
-
-// targetTouches - количество пальцев взаимодействующих с конкретным элементом
-
-// changedTouches - список пальцев, участвующих в текущем событии
+loadScript("js/test.js");
+loadScript("js/some.js");
