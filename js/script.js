e735af8777af048278bc1f1890b746cb9a0ed726
Dmitry <dva160972@gmail.com>
2021-03-18 14:33:15 +0300

    Constructor functions

--- js/script.js
+++ js/script.js
@@ -1,144 +1,39 @@
-window.addEventListener('DOMContentLoaded', () => {
-	
-	// Tabs
-	const tabs = document.querySelectorAll('.tabheader__item'),
-			tabsContent = document.querySelectorAll('.tabcontent'),
-			tabsParent = document.querySelector('.tabheader__items');
+'use strict';
 
-	function hideTabContent() {
-		tabsContent.forEach(item => {
-			item.classList.add('hide');
-			item.classList.remove('show', 'fade');
-		});
+function User(name, id) {
+	this.name = name;
+	this.id = id;
+	this.human = true;
+	this.hello = function() {
+		console.log(`Hello ${this.name}`);
+	};
+}
 
-		tabs.forEach(item => {
-			item.classList.remove('tabheader__item_active');
-		});
+class User {
+	constructor(name, id) {
+		this.name = name;
+		this.id = id;
+		this.human = true;
 	}
-
-	function showTabContent(i = 0) {
-		tabsContent[i].classList.add('show', 'fade');
-		tabsContent[i].classList.remove('hide');
-		tabs[i].classList.add('tabheader__item_active');
+	hello() {
+		console.log(`Hello! ${this.name}`)
 	}
-
-	hideTabContent();
-	showTabContent();
-
-	tabsParent.addEventListener('click', (event) => {
-		const target = event.target;
-
-		if (target && target.classList.contains('tabheader__item')) {
-			tabs.forEach((item, i) => {
-				if (target == item) {
-					hideTabContent();
-					showTabContent(i);
-				}
-			});
-		}
-	});
-
-	// Timer
-
-	const deadLine = '2021-03-17';
-
-	function getTimeRemaining(endtime) {
-		const t = Date.parse(endtime) - Date.parse(new Date()),
-				days = Math.floor(t / (1000 * 60 * 60 * 24)),
-				hours = Math.floor((t / (1000 * 60 * 60) % 24)),
-				minutes = Math.floor((t / 1000 / 60) % 60),
-				seconds = Math.floor((t / 1000) % 60);
-
-		return {
-			'total': t,
-			'days': days,
-			'hours': hours,
-			'minutes': minutes,
-			'seconds': seconds
-		};
+	exit() {
+		console.log(`Пщльзователь ${this.name} ушел`)
 	}
+}
 
-	function getZero(num) {
-		if (num >= 0 && num < 10) {
-			return `0${num}`;
-		} else {
-			return num;
-		}
-	}
+User.prototype.exit = function() {
+	console.log(`Пользователь ${this.name} ушел`);
+};
 
-	function setClock(selector, endtime) {
-		const timer = document.querySelector(selector),
-				days = timer.querySelector('#days'),
-				hours = timer.querySelector('#hours'),
-				minutes = timer.querySelector('#minutes'),
-				seconds = timer.querySelector('#seconds'),
-				timeInterval = setInterval(updateClock, 1000);
+const ivan = new User('Ivan', 20);
+const alex = new User('Alex', 28);
 
-		updateClock();
-		
-		function updateClock() {
-			const t = getTimeRemaining(endtime);
+ivan.exit();
 
-			days.innerHTML = getZero(t.days);
-			hours.innerHTML = getZero(t.hours);
-			minutes.innerHTML = t.minutes;
-			seconds.innerHTML = t.seconds;
+ivan.hello();
+alex.hello();
 
-			if (t.total <= 0) {
-				clearInterval(timeInterval);
-			}
-		}
-	}
-
-	setClock('.timer', deadLine);
-
-	// Modal
-
-	const modalTrigger = document.querySelectorAll('[data-modal]'),
-			modal = document.querySelector('.modal'),
-			modalCloseBtn = document.querySelector('[data-close]');
-
-	function openModal() {
-		modal.classList.add('show');
-		modal.classList.remove('hide');
-		// modal.classList.toggle('show');
-		document.body.style.overflow = 'hidden';
-		clearInterval(modalTimerId);
-	}
-
-	modalTrigger.forEach(btn => {
-		btn.addEventListener('click', openModal);
-	});
-	
-	function closeModal() {
-		modal.classList.add('hide');
-		modal.classList.remove('show');
-		// modal.classList.toggle('show');
-		document.body.style.overflow = '';
-	}
-
-	modalCloseBtn.addEventListener('click', closeModal);
-
-	modal.addEventListener('click', (e) => {
-		if (e.target === modal) {
-			closeModal();
-		}
-	});
-
-	document.addEventListener('keydown', (e) => {
-		if (e.code === "Escape" && modal.classList.contains('show')) {
-			closeModal();
-		}
-	});
-
-	const modalTimerId = setTimeout(openModal, 5000);
-
-	function showModalByScrol() {
-		if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
-			openModal();
-			window.removeEventListener('scroll', showModalByScrol);
-		}
-	}
-
-	window.addEventListener('scroll', showModalByScrol);
-});
+console.log(ivan);
+console.log(alex);
