e1b6859b5cd611610c6d9027b49ce3d69424d0d7
Dmitry <dva160972@gmail.com>
2021-04-27 12:31:06 +0300

    Jquery

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,31 @@
 let path = require('path');
 
 module.exports = {
-	mode: 'development',
-	entry: './js/script.js',
-	output: {
-		filename: 'bundle.js',
-		path: __dirname + '/js'
-	},
-	watch: true,
+  mode: 'production',
+  entry: './script.js',
+  output: {
+    filename: 'bundle.js'
+  },
+  watch: true,
 
-	devtool: "source-map",
+  devtool: "source-map",
 
-	module: {}
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', {
+                debug: true,
+                corejs: 3,
+                useBuiltIns: "usage"
+            }]]
+          }
+        }
+      }
+    ]
+  }
 };
